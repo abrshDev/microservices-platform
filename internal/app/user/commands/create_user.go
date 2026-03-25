747bf9f66fd404152c4c5cb52e0b3ea70d561be8
abrshDev <abrshmelkamu3@gmail.com>
2026-03-25 21:56:52 +0300

    removed comment

--- internal/app/user/commands/create_user.go
+++ internal/app/user/commands/create_user.go
@@ -8,7 +8,6 @@ import (
 	"github.com/abrshDev/user-service/internal/domain/repositories"
 )
 
-// CreateUserRequest defines the data needed to create a user
 type CreateUserRequest struct {
 	Username string
 	Email    string
@@ -23,17 +22,14 @@ func NewCreateUserHandler(repo repositories.UserRepository) *CreateUserHandler {
 }
 
 func (h *CreateUserHandler) Execute(ctx context.Context, req CreateUserRequest) error {
-	// 1. Validation (Business Rules)
 	if req.Username == "" || req.Email == "" {
 		return errors.New("username and email cannot be empty")
 	}
 
-	// 2. Convert Request to Domain Entity
 	user := &entities.User{
 		Username: req.Username,
 		Email:    req.Email,
 	}
 
-	// 3. Use the Repository to save
 	return h.repo.Create(ctx, user)
 }
