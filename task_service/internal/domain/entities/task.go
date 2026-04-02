f96521db61fffe7c31719b6da96277717ed48eca
abrshDev <abrshmelkamu3@gmail.com>
2026-04-02 12:44:18 +0300

    feat(task-service): define domain entities and repository interfaces

---
+++ task_service/internal/domain/entities/task.go
@@ -0,0 +1,26 @@
+package entities
+
+import (
+	"time"
+
+	uuid "github.com/jackc/pgx/pgtype/ext/gofrs-uuid"
+)
+
+type TaskStatus string
+
+const (
+	StatusPending   TaskStatus = "PENDING"
+	StatusRunning   TaskStatus = "RUNNING"
+	StatusCompleted TaskStatus = "COMPLETED"
+	StatusFailed    TaskStatus = "FAILED"
+)
+
+type Task struct {
+	ID          uuid.UUID `gorm:"type:uuid;primary_key;"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null;index"`
+	Title       string    `gorm:"not null"`
+	Description string
+	Status      TaskStatus `gorm:"type:varchar(20);default:'PENDING'"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
