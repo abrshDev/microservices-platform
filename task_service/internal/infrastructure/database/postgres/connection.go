ec175d75be87c5b0dce88efb99b8e31376955ab0
abrshDev <abrshmelkamu3@gmail.com>
2026-04-02 17:58:04 +0300

    feat(infra): add multi-stage Dockerfile and finalize multi-service compose configuration

--- task_service/internal/infrastructure/database/postgres/connection.go
+++ task_service/internal/infrastructure/database/postgres/connection.go
@@ -10,23 +10,23 @@ import (
 	"gorm.io/gorm"
 )
 
-// NewConnection initializes the Postgres DB and runs migrations
 func NewConnection() (*gorm.DB, error) {
-	// Pulling from .env (which will be provided by Docker Compose)
+	// FIX: Changed to TASK_ prefixed variables to match your .env/compose
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
+		os.Getenv("TASK_DB_HOST"),
+		os.Getenv("TASK_DB_USER"),
+		os.Getenv("TASK_DB_PASSWORD"),
+		os.Getenv("TASK_DB_NAME"),
+		os.Getenv("TASK_DB_PORT"),
 	)
+
+	log.Printf("Connecting to Task DB: host=%s dbname=%s", os.Getenv("TASK_DB_HOST"), os.Getenv("TASK_DB_NAME"))
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to database: %v", err)
 	}
 
-	// Senior Move: Auto-Migrate the entities defined in the Domain
 	log.Println("Running database migrations...")
 	err = db.AutoMigrate(&entities.Task{})
 	if err != nil {
