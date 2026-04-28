313115a55813cb32a11d8b59a87de29b60b398b0
abrshDev <abrshmelkamu3@gmail.com>
2026-04-28 12:00:40 +0300

    feat(repo): implement transactional update with audit log

--- reporting_service/internal/domain/repositories/summary_repository.go
+++ reporting_service/internal/domain/repositories/summary_repository.go
@@ -3,6 +3,7 @@ package repositories
 import "github.com/abrshDev/reporting-service/internal/domain/entities"
 
 type SummaryRepo interface {
+	UpdateWithAudit(userID string, tenantID uint64, change int) error
 	UpsertSummary(summary entities.UserTaskSummary) error
 	GetSummary(userID string, tenantID uint64) (*entities.UserTaskSummary, error)
 }
