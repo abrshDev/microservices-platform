4b7ab729e16215c35a5f0057d30277fd949aa1aa
abrshDev <abrshmelkamu3@gmail.com>
2026-04-30 22:01:46 +0300

    feat(reporting-service): track processed event status in repository

--- reporting_service/internal/domain/repositories/summary_repository.go
+++ reporting_service/internal/domain/repositories/summary_repository.go
@@ -1,9 +1,14 @@
 package repositories
 
-import "github.com/abrshDev/reporting-service/internal/domain/entities"
+import (
+	"context"
+
+	"github.com/abrshDev/reporting-service/internal/domain/entities"
+)
 
 type SummaryRepo interface {
 	UpdateWithAudit(userID string, tenantID uint64, change int, actionType string) error
-
+	InsertIfNotExist(ctx context.Context, eventID string) (bool, error)
+	UpdateStatus(ctx context.Context, eventID string, status string) error
 	GetSummary(userID string, tenantID uint64) (*entities.UserTaskSummary, error)
 }
